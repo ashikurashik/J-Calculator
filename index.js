32df90359074dcd45cefcb50eb9139b3fb34dd40
Md. Ashikur Rahman <86947608+ashikurashik@users.noreply.github.com>
2025-04-14 09:28:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function Solve(val) {
        Result();
     } else if (key === 'Backspace') {
        Back();
-    } else if (key.toLowerCase() === 'Delete') {
+    } else if (key.toLowerCase() === 'c') {
        Clear();
     }
  });
